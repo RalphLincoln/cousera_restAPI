f3ccc8b9271486ed57bc3aeff5170681ab4cdc72
Alaneme Ikenna Raphael <ialaneme@yahoo.com>
2020-09-27 04:50:30 +0100

    express-session 2

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 
-// MODELS
-// const Dishes = require('./models/dishes');
 
-
+// CONNECTING TO MONGODB
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
@@ -47,45 +45,25 @@ app.use(session({
 }));
 
 
+// THIS END POINTS WILL OCCUR BEFORE THE AUTHENTICATION
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 const auth = (req, res, next) => {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      var err = new Error("You are not authenticated")
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === "admin" && password === "password") {
-      req.session.user = 'admin';
-      next();
-    }
-    else {
-      var err = new Error("You are not authenticated")
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    var err = new Error("You are not authenticated")
+    err.status = 401;
+    return next(err);
   }
   else {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     }
     else {
       var err = new Error("You are not authenticated")
-
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -93,14 +71,11 @@ const auth = (req, res, next) => {
 }
 
 
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(auth);
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// THIS PART CAN ONLY BE ACCESSED AFTER THE AUTHENTICATION
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,103 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const User = require('../models/users');
+
 const userRouter = express.Router();
 
 userRouter.use(bodyParser.json());
 
-userRouter.route('/')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
+// userRouter.route('/')
+//     .all((req, res, next) => {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'text/plain');
+//         next();
+//     })
+//     .get((req, res, next) => {
+//         res.end('Will send the users to you!');
+//     })
+
+userRouter.route('/signup')
+    .post((req, res, next) => {
+        // FIRST CHECK TO SEE IF THAT USERNAME ALREADY EXISTS
+        User.findOne({ username: req.body.username })
+            .then(user => {
+                if (user !== null) {
+                    var err = new Error(`User ${req.body.username} already exists`);
+                    err.status = 403;
+                    next(err);
+                }
+                else {
+                    return User.create({
+                        username: req.body.username,
+                        password: req.body.password
+                    })
+                }
+            })
+            // THIS IS HANDLING A SENARIO WHERE A USERNAME DOES NOT EXISTS. THE USERNAME WILL BE SAVED TO THE DATABASE
+            .then(user => {
+                res.status(200).json(user)
+            })
+            .catch((err) => next(err))
     })
-    .get((req, res, next) => {
-        res.end('Will send the users to you!');
+
+userRouter.route('/login')
+    .post((req, res, next) => {
+        // THIS IF CHECK TRIGGERS WHEN THERE IS NO EXISTING SESSION
+        if (!req.session.user) {
+            var authHeader = req.headers.authorization;
+
+            if (!authHeader) {
+                var err = new Error("You are not authenticated")
+
+                res.setHeader("WWW-Authenticate", "Basic");
+                err.status = 401;
+                return next(err);
+            }
+
+            var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+
+            var username = auth[0];
+            var password = auth[1];
+
+            User.findOne({ username: username })
+                .then(user => {
+                    if (user === null) {
+                        var err = new Error(`User ${username} does not exists`);
+                        err.status = 403;
+                        return next(err);
+                    }
+                    else if (user.password !== password) {
+                        var err = new Error('Your password is incorrect');
+                        err.status = 403;
+                        return next(err);
+                    }
+                    else if (user.username === username && user.password === password) {
+                        req.session.user = 'authenticated';
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", 'text/plain');
+                        res.end("You are authenticated");
+                    }
+                })
+                .catch((err) => next(err))
+        }
+        else {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", 'text/plain');
+            res.end("You are already authenticated");
+        }
+    })
+userRouter.route('/logout')
+    .get((req, res) => {
+        if (req.session) {
+            req.session.destroy();
+            res.clearCookie('session-id');
+            res.redirect('/');
+        }
+        else {
+            var err = new Error("You are not logged in!!");
+            err.statusCode = 403;
+            next(err);
+        }
     })
 module.exports = userRouter;
